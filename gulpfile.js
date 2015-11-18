107e8abd4d49b13e8945f03351b221869cad8230
jessiewillms <willms.jessie@gmail.com>
2015-11-17 22:13:08 -0500

    rest of text + styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,43 @@
+// "use strict";
+
 // Include gulp
 var gulp = require('gulp');
 
-// gulp-jshint gulp-sass gulp-concat gulp-uglify gulp-rename
-var jshint = require('gulp-jshint'),
-	sass = require('gulp-sass'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename'),
-    s3 = require("gulp-s3-ls"),
-    aws = require("./aws.json");
-
-gulp.src('./dist/**')
-    .pipe(s3(aws));
-
+// npm install gulp npm install gulp-notify gulp-jshint gulp-sass gulp-concat gulp-uglify gulp-rename gulp-autoprefixer
+var sass = require('gulp-sass'),
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    notify = require("gulp-notify"),
+    autoprefixer = require('gulp-autoprefixer');
+// 
+    
 // Lint Task
 gulp.task('lint', function() {
     return gulp.src('js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
-
 // Compile Our Sass
-gulp.task('sass', function() {
-    return gulp.src('styles/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('styles'));
+gulp.task('css-task', function() {
+  return gulp.src('styles/*.scss')
+    .pipe(sass({
+        'sourcemap=none': true,
+        errLogToConsole: true
+    }))
+    .on("error", notify.onError(function(error) {
+        return "Message to the notifier: " + error.message;
+    }))
+    .pipe(sass().on('error', sass.logError))
+    // .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(gulp.dest('styles'))
+    .pipe(notify('You are good at programming and have nice hair!'))
 });
 
+
 // Concatenate & Minify JS
-gulp.task('scripts', function() {
+gulp.task('scripts-task', function() {
     return gulp.src('js/*.js')
         .pipe(concat('all.js'))
         .pipe(gulp.dest('dist'))
@@ -39,9 +48,9 @@ gulp.task('scripts', function() {
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['lint', 'scripts']);
-    gulp.watch('styles/*.scss', ['sass']);
+    gulp.watch('js/*.js', ['lint', 'scripts-task']);
+    gulp.watch('styles/*.scss', ['css-task']);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+gulp.task('default', ['css-task', 'lint', 'scripts-task', 'watch']);
